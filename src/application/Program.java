9e41b782ffac5588922f38d234c3febe7e6cae6c
Tiago VC <vc.tiago@gmail.com>
2023-01-12 16:11:37 -0300

    implementing delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,36 +13,41 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		System.out.println("==== TEST 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n==== TEST 2: seller findByDepartment ====");
-		Department department = new Department (2, null);
+		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
+
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n==== TEST 3: seller findAll ====");
 		list = sellerDao.findAll();
-		
+
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n==== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
-		
-		
+
 		System.out.println("\n==== TEST 5: seller update ====");
 		seller = sellerDao.findById(1);
 		seller.setName("Bob Brown");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+
+		System.out.println("\n==== TEST 6: seller delete ====");
+		sellerDao.deleteById(13);
+		System.out.println("Delete executed!");
+
 	}
 
 }
